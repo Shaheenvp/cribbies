a302922a89278f10e3e155853c443e13584d4318
gaseer <gaseermohammed8@gmail.com>
2024-02-22 10:45:48 +0530

    connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:cribbies/splash.dart';
+import 'package:cribbies/views/SignIn/login.dart';
 import 'package:cribbies/views/SignUp/SignUp.dart';
 import 'package:cribbies/views/add_itempage/add_item.dart';
 import 'package:cribbies/views/detail_page/detail_page.dart';
 import 'package:cribbies/views/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,11 +21,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: DetailPage(),
-        debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      home: SignIn(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-
